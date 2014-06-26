e336676d22c4eaa804ca6da3647ba609b3ef1525
Jed Parsons <jedp@me.com>
2014-06-26 12:33:39 -0700

    confirm that http server works

---
+++ http/http.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+  console.log('got a request');
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify({message: "Hello, world"}));
+}).listen(80);
+
+console.log('Server running');
