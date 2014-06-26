eb86064695d4ea4d61c6ff230329b4dfd2111300
Jed Parsons <jedp@me.com>
2014-06-25 21:43:47 -0700

    Event listeners only work on digital pins (GPIO)

--- motion/motion.js
+++ motion/motion.js
@@ -1,14 +1,11 @@
 // Receive motion detector data on GPIO A6 (pin 4)
 
 var tessel = require('tessel');
-var motion = tessel.port.GPIO.pin.A6;
+var motion = tessel.port.GPIO.pin.G6;
 
 // It works!
-console.log('pin A6:', motion.read());
+console.log('pin G6:', motion.read());
 
-// XXX registering an .on() event listener fails
-// Opened tessel forum topic:
-// https://forums.tessel.io/t/gpio-events-cannot-call-method-on/302
 motion.on('change', function(time, type) {
   console.log('motion:', time, type, motion.read());
 });
